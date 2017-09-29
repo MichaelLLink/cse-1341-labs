38e067c21486e9258104fcf80c42b31264139116
MichaelLLink <32107125+MichaelLLink@users.noreply.github.com>
2017-09-28 19:48:33 -0500

    new

--- Lab 2/TargetProfit.java
+++ Lab 2/TargetProfit.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class TargetProfit {
+class TargetProfit {
 public static void main(String[] args)
 {
 	Scanner tpinput = new Scanner(System.in);
@@ -7,8 +7,8 @@ public static void main(String[] args)
 	double profitMargin = tpinput.nextDouble();
 	System.out.println("What is the cost of the product?");
 	double cost = tpinput.nextDouble();
-	double price = cost * (cost * (profitMargin/100));
-	System.out.printf("The target price is $ %5.2f", price);
+	double price = cost * (cost + (cost*profitMargin));
+	System.out.printf("The target price is $%d", price);
 }
 
 }
